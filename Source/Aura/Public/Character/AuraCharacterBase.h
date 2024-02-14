6fba64593056dd93f28e8e2ced4cd2200380ed21
eureka139 <eureka139@outlook.com>
2024-02-14 16:58:08 +0800

    2021.2.14 AuraAndEnemyBP

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -12,18 +12,15 @@ class AURA_API AAuraCharacterBase : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	AAuraCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere,Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
