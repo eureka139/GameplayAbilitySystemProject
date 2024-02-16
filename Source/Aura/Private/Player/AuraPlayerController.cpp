bddcc2a6f2b627eb23a8a25100f6bae665ac3b3f
eureka139 <eureka139@outlook.com>
2024-02-16 20:02:09 +0800

    2024.2.16 SetupInputComponent

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -29,3 +29,33 @@ void AAuraPlayerController::BeginPlay()
 	
 	SetInputMode(InputModeData);
 }
+
+void AAuraPlayerController::SetupInputComponent()
+{
+
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+
+	const FRotator Rotation = GetControlRotation();
+
+	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.X);
+	}
+}
